8d6e49ae913d025ce51a839578c05c5974d10867
ht9 <njivarizo.job@gmail.com>
2021-09-12 16:03:03 +0300

    General api skelton

--- routes/api.php
+++ routes/api.php
@@ -5,20 +5,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/post', function (Request $request) {
     $validatedData = $request->validate([
-        'id'          => ['required', 'unique:posts', 'max:255'],
-        'title'       => ['required'],
-        'description' => ['required'],
-        'content'     => ['required'],
+        'title'       => 'required|max:255',
+        'description' => 'required|max:255',
+        'website_url' => 'required'
     ]);
 
-
+    // queue_email_to_subsribers
 });
 
 Route::post('/subscription', function (Request $request) {
     $validatedData = $request->validate([
-        'post_id'          => ['required', 'unique:posts', 'max:255'],
-        'subscriber_id'    => ['required']
+        'post_id'          => 'required',
+        'subscriber_email' => 'required',
+        'website_url'      => 'required'
     ]);
 
+    // subsribe_user
 
 });
